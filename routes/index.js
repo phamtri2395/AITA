d9e1ddec062b219c188bc2b6bfd5f1df48f677ee
Tan Nguyen <tannd1993@gmail.com>
2015-12-07 23:27:48 +0700

    route for login page

--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,7 @@ exports = module.exports = function(app) {
 	
 	// Views
 	app.get('/', routes.views.index);
+	app.get('/login', routes.views.login);
 	app.get('/blog/:category?', routes.views.blog);
 	app.get('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
