e92f03bc6bfe7f1161a5150345b37629271ef237
Tam Pham <tampham47@live.com>
2016-01-09 11:32:39 +0700

    update fb to post sample content

--- routes/views/fb.js
+++ routes/views/fb.js
@@ -6,26 +6,27 @@ var FB = require('fb');
 FB.setAccessToken(process.env.FACEBOOK_TOKEN);
 
 exports = module.exports = function(req, res) {
+	//ref from https://developers.facebook.com/docs/graph-api/reference/post#publishing
+	var message = {
+		message: 'Chúng tôi cần Bán và cho Thuê căn hộ cao cấp Sun Rise City tại quận 7, nằm tại trục đường Nguyễn Hữu Thọ liên kết với các quận 1, 4, 5, 8. \nMặt tiền đường Nguyễn Hữu Thọ, đối diện siêu thị Lotte Quận 7.',
 
-	FB.api('528623837288211/feed', 'post', { message: 'gọi từ api' + Date.now.toString()}, function (err, result) {
-		if(!result || result.error) {
-			console.log(!result ? 'error occurred' : result.error);
-			res.json(result);	
-			return;
+		// name of the link
+		name: 'Aita.vn',
+		link: 'http://aitavn.herokuapp.com/chi-tiet/',
+		picture: 'http://file4.batdongsan.com.vn/resize/745x510/2015/06/04/20150604150037-9e21.jpg',
+		description: 'Nếu biết tình như thế, chẳng lớn lên làm gì',
+		caption: 'aita.vn',
+
+		// place: 'Sai gon',
+		story: 'Chuyện tình dưới mưa, tần khánh',
+		type: 'photo'
+	};
+
+	FB.api('528623837288211/feed', 'post', message, function (result) {
+		if(result.error) {
+			res.json({err: result.error});	
+		} else {
+			res.json(result);
 		}
-		console.log(err, result);
-		// console.log('post Id: ' + result.id);
-		res.json(result);
 	});
-
-	// var view = new keystone.View(req, res);
-	// var locals = res.locals;
-	// locals.user = req.user;
-
-	// // locals.section is used to set the currently selected
-	// // item in the header navigation.
-	// locals.section = 'home';
-
-	// // Render the view
-	// view.render('chi-tiet');
 };
