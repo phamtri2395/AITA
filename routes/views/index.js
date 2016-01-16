16121023c7674adb0269d9c2211be381ae42d003
phamtri2395 <phamtri2395@gmail.com>
2016-01-16 11:11:59 +0700

    integrate category menu

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,4 +1,5 @@
 var keystone = require('keystone');
+var async = require('async');
 
 exports = module.exports = function(req, res) {
 	
@@ -10,6 +11,54 @@ exports = module.exports = function(req, res) {
 	// item in the header navigation.
 	locals.section = 'home';
 	
+	// Init locals's data
+	locals.data = {
+		posts: [],
+		categories: []
+	};
+
+	// Load all categories
+	view.on('init', function(next) {
+		
+		keystone.list('Category').model.find().sort('name').exec(function(err, results) {
+			
+			if (err || !results.length) {
+				return next(err);
+			}
+			
+			locals.data.categories = results;
+
+			// Load the counts for each category
+			async.each(locals.data.categories, function(category, next) {
+				
+				keystone.list('Post').model.count().where('categories').in([category.id]).exec(function(err, count) {
+					category.postCount = count;
+					next(err);
+				});
+				
+			}, function(err) {
+				next(err);
+			});
+						
+		});
+		
+	});
+
+	// Load the current category filter
+	view.on('init', function(next) {
+		
+		if (req.params.category) {
+			keystone.list('Category').model.findOne({ key: locals.filters.category }).exec(function(err, result) {
+				locals.data.category = result;
+				next(err);
+			});
+		} else {
+			next();
+		}
+		
+	});
+
+
 	// Render the view
 	view.render('index');
 	
