4885c5ef87d841359307a7cb855e7540b5d72227
phamtri2395 <phamtri2395@gmail.com>
2016-01-17 19:34:15 +0700

    integrate home page

--- routes/views/index.js
+++ routes/views/index.js
@@ -15,7 +15,7 @@ exports = module.exports = function(req, res) {
 	locals.data = {
 		districts: [],
 		posts: [],
-		categories: []
+		categories: [],
 	};
 
 	// Load all categories
@@ -32,7 +32,7 @@ exports = module.exports = function(req, res) {
 			// Load the counts for each category
 			async.each(locals.data.categories, function(category, next) {
 				
-				keystone.list('Category').model.count().where('category').in([category.id]).exec(function(err, count) {
+				keystone.list('Category').model.count().exec(function(err, count) {
 					category.postCount = count;
 					next(err);
 				});
@@ -59,7 +59,7 @@ exports = module.exports = function(req, res) {
 			// Load the counts for each district
 			async.each(locals.data.districts, function(district, next) {
 				
-				keystone.list('District').model.count().where('district').in([district.id]).exec(function(err, count) {
+				keystone.list('District').model.count().exec(function(err, count) {
 					district.postCount = count;
 					next(err);
 				});
@@ -86,7 +86,7 @@ exports = module.exports = function(req, res) {
 			// Load the counts for each post
 			async.each(locals.data.posts, function(post, next) {
 				
-				keystone.list('Post').model.count().where('post').in([post.id]).exec(function(err, count) {
+				keystone.list('Post').model.count().exec(function(err, count) {
 					post.postCount = count;
 					next(err);
 				});
