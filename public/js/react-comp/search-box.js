30b1985ffc06f3bc7a093f11deb870db8782b360
Tam Pham <tampham47@live.com>
2016-03-18 22:00:18 +0700

    init react into project

---
+++ public/js/react-comp/search-box.js
@@ -0,0 +1,14 @@
+/**
+ * Capheshift 2016
+ * Author: Tw
+ */
+
+
+if ($('.js-container').hasClass('js-page-index')) {
+
+	ReactDOM.render(
+		<span>react search box</span>,
+		document.getElementById('search-box-holder')
+	);
+}
+
