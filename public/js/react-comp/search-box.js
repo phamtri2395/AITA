87f37d89cf9a18b22e2f48744179bd427cbe2d26
Tam Pham <tampham47@live.com>
2016-03-18 23:15:41 +0700

    add react-select

--- public/js/react-comp/search-box.js
+++ public/js/react-comp/search-box.js
@@ -3,12 +3,43 @@
  * Author: Tw
  */
 
+/*global $, React, Select */
 
 if ($('.js-container').hasClass('js-page-index')) {
 
-	ReactDOM.render(
-		<span>react search box</span>,
+
+	var Container = React.createClass({
+		getInitialState: function() {
+			var options = [
+				{ label: 'One', value: 1 },
+				{ label: 'Two', value: 2 },
+				{ label: 'Three', value: 3 },
+			];
+
+			return { 
+				value: 1,
+				options: options
+			};
+		},
+
+		updateValue: function(value) {
+			this.setState({ value: value });
+		},
+
+		render: function() {
+			return (
+				<Select
+			    name="form-field-name"
+			    value={this.state.value}
+			    options={this.state.options}
+			    onChange={this.updateValue} />
+			);
+		}
+	});
+
+	React.render(
+		React.createElement(Container),
 		document.getElementById('search-box-holder')
 	);
-}
 
+}
